06b004ee59adc294d4a2578c841c09d3d10f9607
Ryan Huard <ryan.huard@live.ca>
2014-11-27 09:49:13 -0700

    no message

--- notMenu.h
+++ notMenu.h
@@ -15,19 +15,19 @@ void drawMainMenu()
 	tft.setTextColor(RED);
 	tft.print(nameString);
     // Match start
-    tft.drawRect(34, 80, 30, 12, WHITE);
-    tft.setCursor(36, 80);
+    tft.drawRect(44, 80, 40, 12, WHITE);
+    tft.setCursor(50, 83);
     tft.setTextColor(WHITE);
     tft.print("Start");
     // Choose music
-    tft.drawRect(34, 100, 30, 12, WHITE);
+    tft.drawRect(44, 100, 40, 12, WHITE);
     // Draw music symbol so good
-    tft.fillCircle(100, 100, 4, WHITE);
-    tft.fillRect(100, 90, 2, 10, WHITE);
-    tft.drawLine(100, 90, 103, 93, WHITE);
-    tft.drawLine(101, 90, 104, 93, WHITE);
-    tft.drawLine(102, 90, 105, 93, WHITE);
-    tft.setCursor(36, 100);
+    tft.fillCircle(96, 100, 4, WHITE);
+    tft.fillRect(96, 96, 2, 10, WHITE);
+    tft.drawLine(95, 96, 98, 99, WHITE);
+    tft.drawLine(96, 96, 99, 99, WHITE);
+    tft.drawLine(97, 96, 100, 99, WHITE);
+    tft.setCursor(50, 103);
     tft.print("Music");
 
 }
